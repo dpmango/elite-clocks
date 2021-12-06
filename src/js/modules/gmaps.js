a2475d823927f384b822e0f8d4c6467d29b47c8b
Sergey Khmelevskoy <xs290@me.com>
2021-12-06 12:05:25 +0300

    init homepage

---
+++ src/js/modules/gmaps.js
@@ -0,0 +1,187 @@
+//////////
+// GMAPS
+//////////
+(function ($, APP) {
+  APP.Plugins.Gmaps = {
+    data: {
+      scriptsCreated: false,
+      gmapsLoaded: false,
+    },
+    init: function () {
+      if ($('.js-gmap').length > 0) {
+        if (this.data.gmapsLoaded) {
+          // google.maps.event.addDomListener(window, 'load', this.initMaps.bind(this));
+          this.initMaps();
+        } else {
+          this.tryLoadScripts();
+        }
+      }
+    },
+    createScripts: function () {
+      var gmapsK = 'AIzaSyDLIO63kPkzTp9XBGwuaau9J-xVyCzL5m8';
+      var gmapsScript = document.createElement('script');
+      gmapsScript.type = 'text/javascript';
+      gmapsScript.src = `https://maps.googleapis.com/maps/api/js?key=${gmapsK}`;
+      $('head').append(gmapsScript);
+      this.data.scriptsCreated = true;
+    },
+    tryLoadScripts: function () {
+      var _this = this;
+      if (!_this.data.scriptsCreated) {
+        _this.createScripts();
+      }
+
+      var ticker = setInterval(readyChecker, 250);
+      function readyChecker() {
+        if (!_this.data.gmapsLoaded) {
+          try {
+            if (google && google.maps && google.maps.event) {
+              _this.data.gmapsLoaded = true;
+              _this.init(); // reinit
+              clearInterval(ticker);
+            }
+          } catch (e) {
+            // console.log('maps not ready yeat, another try');
+          }
+        }
+      }
+    },
+    initMaps: function () {
+      var _this = this;
+      $('.js-gmap').each(function (i, domElement) {
+        _this.drawMap(domElement);
+      });
+    },
+    drawMap: function (domElement) {
+      var _this = this;
+      var $domElement = $(domElement);
+      if ($domElement.length === 0) return;
+
+      var myMap;
+      const center = _this.geoStringToArr($domElement.data('center'));
+
+      var params = {
+        center: new google.maps.LatLng(center[0], center[1]),
+        zoom: $domElement.data('zoom') || 10,
+        styles: [
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{ color: '#e9e9e9' }, { lightness: 17 }],
+          },
+          {
+            featureType: 'landscape',
+            elementType: 'geometry',
+            stylers: [{ color: '#f5f5f5' }, { lightness: 20 }],
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [{ color: '#ffffff' }, { lightness: 17 }],
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#ffffff' }, { lightness: 29 }, { weight: 0.2 }],
+          },
+          {
+            featureType: 'road.arterial',
+            elementType: 'geometry',
+            stylers: [{ color: '#ffffff' }, { lightness: 18 }],
+          },
+          {
+            featureType: 'road.local',
+            elementType: 'geometry',
+            stylers: [{ color: '#ffffff' }, { lightness: 16 }],
+          },
+          {
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [{ color: '#f5f5f5' }, { lightness: 21 }],
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{ color: '#dedede' }, { lightness: 21 }],
+          },
+          {
+            elementType: 'labels.text.stroke',
+            stylers: [{ visibility: 'on' }, { color: '#ffffff' }, { lightness: 16 }],
+          },
+          {
+            elementType: 'labels.text.fill',
+            stylers: [{ saturation: 36 }, { color: '#333333' }, { lightness: 40 }],
+          },
+          { elementType: 'labels.icon', stylers: [{ visibility: 'off' }] },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{ color: '#f2f2f2' }, { lightness: 19 }],
+          },
+          {
+            featureType: 'administrative',
+            elementType: 'geometry.fill',
+            stylers: [{ color: '#fefefe' }, { lightness: 20 }],
+          },
+          {
+            featureType: 'administrative',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#fefefe' }, { lightness: 17 }, { weight: 1.2 }],
+          },
+        ],
+      };
+
+      // placeholder: {
+      //   geodata: _this.geoStringToArr($domElement.data('placeholder')),
+      //   caption: $domElement.data('placeholder-caption'),
+      //   balloon: $domElement.data('placeholder-balloon'),
+      // },
+
+      if (!params.center) return;
+
+      // CREATE MAP INSTANCE
+      console.log('drawing', params);
+      myMap = new google.maps.Map(domElement, params);
+
+      // CONTROLS
+      // myMap.controls.remove('trafficControl');
+      // myMap.controls.remove('searchControl');
+      // myMap.controls.remove('fullscreenControl');
+      // myMap.controls.remove('rulerControl');
+      // myMap.controls.remove('geolocationControl');
+      // myMap.controls.remove('routeEditor');
+      // myMap.controls.remove('typeSelector');
+      // myMap.controls.remove('zoomControl');
+
+      // PLACEHOLDER
+      if (params.placeholder.geodata) {
+        var placeholder = new ymaps.Placemark(
+          params.placeholder.geodata,
+          {
+            balloonContent: params.placeholder.balloon,
+            iconCaption: params.placeholder.caption,
+          },
+          {
+            preset: 'islands#redIcon',
+          }
+        );
+
+        // var marker = new google.maps.Marker({
+        //   position: new google.maps.LatLng(40.67, -73.94),
+        //   map: map,
+        //   title: 'Snazzy!',
+        // });
+
+        myMap.geoObjects.add(placeholder);
+      }
+    },
+    geoStringToArr: function (str) {
+      var split = str.split(',');
+      if (split.length === 2) {
+        return [parseFloat(split[0]), parseFloat(split[1])];
+      }
+
+      return false;
+    },
+  };
+})(jQuery, window.APP);
---
+++ src/js/modules/plugins/sliders.js
@@ -0,0 +1,211 @@
+//////////
+// SLIDERS
+//////////
+(function ($, APP) {
+  APP.Plugins.Sliders = {
+    data: {
+      swipers: {
+        hero: undefined,
+        product: undefined,
+        cgallery: undefined,
+      },
+      responsiveSwipers: {
+        backstageSwiper: {
+          instances: [],
+          enableOn: 768,
+        },
+      },
+    },
+    init: function (fromPjax) {
+      if (!fromPjax) {
+        this.initSwipers();
+        this.initSwiperDataTree();
+        this.initResponsiveSwipers();
+      }
+      this.listenResize();
+    },
+    utils: {
+      // builder helpers
+      buildProps: function (name, options, $dom) {
+        const defaultProps = {
+          watchOverflow: true,
+          setWrapperSize: false,
+          slidesPerView: 'auto',
+          normalizeSlideIndex: true,
+          slideToClickedSlide: true,
+          touchEventsTarget: 'wrapper',
+          threshold: 10,
+        };
+
+        // optional props
+        let oProps = {};
+        if (options && options.pagination) {
+          oProps = {
+            pagination: {
+              el: `.swiper-${name}-pagination`,
+              type: 'bullets',
+              clickable: true,
+            },
+          };
+        }
+
+        if (options && options.navigation) {
+          oProps = {
+            ...oProps,
+            navigation: {
+              nextEl: `.swiper-${name}-next`,
+              prevEl: `.swiper-${name}-prev`,
+            },
+          };
+        }
+
+        // build props from data-
+        let domProps = {};
+        const dataBefore = $dom.data('offset-before');
+        const dataAfter = $dom.data('offset-after');
+        if (dataBefore) {
+          domProps = {
+            slidesOffsetBefore: dataBefore,
+          };
+        }
+        if (dataAfter) {
+          domProps = {
+            ...domProps,
+            slidesOffsetAfter: dataAfter,
+          };
+        }
+
+        return {
+          ...defaultProps,
+          ...oProps,
+          ...domProps,
+        };
+      },
+      buildSwiper: function (name, eProps, options) {
+        const $page = $('.page').last();
+        const $dom = $page.find(`.js-swiper-${name}`);
+
+        if ($dom.length === 0) return;
+
+        let props = APP.Plugins.Sliders.utils.buildProps(name, options, $dom);
+        let swiper = new Swiper(`.js-swiper-${name}:not(.swiper-container-initialized)`, {
+          ...props,
+          ...eProps,
+        });
+        return swiper;
+      },
+    },
+    update: function (selector) {
+      var $swiper;
+      // if selector passed - update only with selector
+      if (selector) {
+        $swiper = $(`${selector}.swiper-container-initialized`);
+      } else {
+        $swiper = $('.swiper-container-initialized');
+      }
+
+      if ($swiper.length > 0) {
+        $swiper.each(function (i, swiper) {
+          $(swiper)[0].swiper.update();
+        });
+      }
+    },
+    listenResize: function () {
+      _window.on('resize', debounce(this.initResponsiveSwipers.bind(this), 200));
+    },
+    initSwipers: function () {
+      var _this = this;
+
+      // hero
+      this.data.swipers.hero = _this.utils.buildSwiper(
+        'hero',
+        {
+          loop: true,
+          spaceBetween: 0,
+          slidesPerView: 1,
+        },
+        { navigation: true, pagination: true }
+      );
+
+      // product
+      this.data.swipers.product = _this.utils.buildSwiper(
+        'product',
+        {
+          loop: true,
+          spaceBetween: 0,
+          slidesPerView: 1,
+        },
+        { navigation: true }
+      );
+
+      // cgallery
+      this.data.swipers.cgallery = _this.utils.buildSwiper(
+        'cgallery',
+        {
+          loop: false,
+          spaceBetween: 20,
+          slidesPerView: 'auto',
+          slidesOffsetAfter: 100,
+        },
+        { navigation: true }
+      );
+    },
+    initSwiperDataTree: function () {
+      var backstageSwiper = '.js-swiper-backstage';
+      if ($(backstageSwiper).length > 0) {
+        this.initSwiperTree(backstageSwiper, 'backstageSwiper');
+      }
+    },
+    initResponsiveSwipers: function () {
+      var backstageSwiper = '.js-swiper-backstage';
+      if ($(backstageSwiper).length > 0) {
+        this.responsiveSwiperConstructor(backstageSwiper, 'backstageSwiper', {
+          watchOverflow: true,
+          setWrapperSize: false,
+          spaceBetween: 0,
+          slidesPerView: 'auto',
+          freeMode: true,
+          // centeredSlides: true,
+          freeModeSticky: false,
+          on: {
+            reachEnd: function (swiper) {
+              if (!APP.Components.Backstage.data.isFetching) {
+                APP.Components.Backstage.getNewBackstageItems();
+                swiper.setTranslate(0);
+              }
+            },
+          },
+        });
+      }
+    },
+    initSwiperTree: function (selector, name) {
+      var _this = this;
+      _this.data.responsiveSwipers[name].instances = [];
+      $(selector).each(function (i, sw) {
+        _this.data.responsiveSwipers[name].instances.push(undefined);
+      });
+    },
+    responsiveSwiperConstructor: function (selector, objName, options) {
+      var dataObj = this.data.responsiveSwipers[objName];
+
+      $(selector).each(function (idx, element) {
+        if (window.innerWidth <= dataObj.enableOn) {
+          if (dataObj.instances[idx] === undefined) {
+            dataObj.instances[idx] = new Swiper(element, options);
+          }
+        } else {
+          if (dataObj.instances[idx] !== undefined) {
+            dataObj.instances[idx].destroy(true, true);
+            dataObj.instances[idx] = undefined;
+          }
+        }
+      });
+
+      this.data.responsiveSwipers[objName] = dataObj;
+    },
+
+    destroy: function () {
+      // ... code ...
+    },
+  };
+})(jQuery, window.APP);
